9ace712b55782b27faf7000f9c6f4c642b7e4bcf
Ganeshbabu26 <ganeshbabumadhavan@gmail.com>
2025-06-22 19:31:28 +0530

    cnahes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-  base: '/QrCode/',
+  base: '/QrCode/', 
   build: {
-    outDir: 'build', 
+    outDir: 'docs', 
     emptyOutDir: true,
     rollupOptions: {
       output: {

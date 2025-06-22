57b60e96fdc4c8902c4c3740d430e52b2ed769ab
Ganeshbabu26 <ganeshbabumadhavan@gmail.com>
2025-06-22 19:20:31 +0530

    new project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/QrCode/',
+  build: {
+    outDir: 'build', 
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'static/js/[name].js',
+        chunkFileNames: 'static/js/[name].js',
+        assetFileNames: 'static/media/[name].[ext]'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true,
+    strictPort: true,
+    host: 'localhost', 
+  }
 })
